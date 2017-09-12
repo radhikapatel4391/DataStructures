2145b7a2d72de60b0dcce876811447e27e52de88
radhikapatel4391 <radhika.patel4391@gmail.com>
2017-09-12 09:39:28 -0400

    Update LinkListAddDeleteUpdate.java

--- src/ds/array/LinkListAddDeleteUpdate.java
+++ src/ds/array/LinkListAddDeleteUpdate.java
@@ -128,4 +128,27 @@ public class LinkListAddDeleteUpdate {
 
 		return headA;
 	}
+	
+    int FindMergeNode(Node headA, Node headB) {
+    Node currentA = headA;
+    Node currentB = headB;
+
+    //Do till the two nodes are the same
+    while(currentA != currentB){
+        //If you reached the end of one list start at the beginning of the other one
+        //currentA
+        if(currentA.next == null){
+            currentA = headB;
+        }else{
+            currentA = currentA.next;
+        }
+        //currentB
+        if(currentB.next == null){
+            currentB = headA;
+        }else{
+            currentB = currentB.next;
+        }
+    }
+    return currentB.data;
+}
 }
